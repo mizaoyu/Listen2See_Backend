28cdee50289a71e7e184e083837b8477842ad2a2
wfnuser <wfnuser@126.com>
2016-11-26 22:52:26 +0800

    [FEATURE] websocket worked

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_socketio import SocketIO
 from www.test import test
 from www.upload import upload
 
@@ -7,6 +8,14 @@ app = Flask(__name__)
 app.register_blueprint(test, url_prefix='/test')
 app.register_blueprint(upload, url_prefix='/upload')
 app.debug = True
+app.config.from_pyfile('config.py')
+socketio = SocketIO(app)
+
+@socketio.on('test')
+def handle_test_message(message):
+    print('received message: ' + message['data'])
+    socketio.emit('testresponse', {'data': 'success'})
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	socketio.run(app, host='0.0.0.0')
--- www/upload.py
+++ www/upload.py
@@ -1,12 +1,31 @@
-from flask import Blueprint
+import os
+from flask import Blueprint, request, redirect, url_for
 
 upload = Blueprint('upload', __name__)
 
 @upload.route('/audio', methods=['GET', 'POST'])
 def audio():
-	return 'Test!'
+	if request.method == 'POST':
+		file = request.files['file']
+		if file:
+			filename = file.filename
+			file.save(os.path.join('./public/audio', filename)) # todo: use config file
+			return 'success'
+	return '''
+	<!doctype html>
+	<title>Upload new File</title>
+	<h1>Upload new File</h1>
+	<form action="" method=post enctype=multipart/form-data>
+	<p><input type=file name=file>
+	<input type=submit value=Upload>
+	</form>
+	'''
 
 @upload.route('/text', methods=['GET', 'POST'])
 def text():
-	return 'Test!'
+	if request.method == 'POST':
+		data = request.data
+		if data:
+			return data
+
 
